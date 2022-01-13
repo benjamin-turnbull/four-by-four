a021ff2084f9c98bac5f0af7d3e6ae56dc8be721
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-13 21:23:36 +1030

    wrote program functionality

---
+++ four-by-four.c
@@ -0,0 +1,13 @@
+/* prints 16 inputted numbers in a four by four arrangement */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen;
+
+    printf("Enter the numbers from 1 to 16 in any order: ");
+    scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &one, &two, &three, &four, &five, &six, &seven, &eight, &nine, &ten, &eleven, &twelve, &thirteen, &fourteen, &fifteen, &sixteen);
+
+    printf("%3d %3d %3d %3d \n%3d %3d %3d %3d \n%3d %3d %3d %3d \n%3d %3d %3d %3d", one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen);
+}
